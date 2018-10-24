1336f1b20bf766934791bf436c23de155581ddd3
anshnagrath <anshnagrath448@gmail.com>
2018-10-24 22:16:29 +0530

    ui fixes

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -15,12 +15,18 @@ export class HomepageComponent {
   constructor() { }
   forSmartHub() {
     this.toScroll = !this.toScroll;
+    this.logins = false;
+    this.creativeGall = false;
+    this.forFinalcial = false;
+    this.toflip = false;
+
   }
   flip() {
     this.toflip = !this.toflip;
     this.logins = false;
     this.creativeGall = false;
     this.forFinalcial = false;
+    this.toScroll = false;
 
 
   }
@@ -29,59 +35,106 @@ export class HomepageComponent {
     this.forFinalcial = !this.forFinalcial;
     this.toflip = false;
     this.creativeGall = false;
+    this.toScroll = false;
+
 
   }
   registration() {
     this.forFinalcial = false;
+    this.GST = false;
+    this.Documentation = false;
+    this.tax = false;
     this.toRegister = !this.toRegister;
+    this.personalService = false;
+    this.complaince = false;
 
   }
   forGST() {
     this.forFinalcial = false;
+    this.toRegister = false;
+    this.Documentation = false;
+    this.tax = false;
     this.GST = !this.GST;
+    this.personalService = false;
+    this.complaince = false;
 
   }
   forDocumentation() {
+    this.complaince = false;
+    this.GST = false;
+    this.toRegister = false;
+    this.toScroll = false;
     this.forFinalcial = false;
+    this.tax = false;
+    this.personalService = false;
     this.Documentation = !this.Documentation;
   }
   forTax() {
+    // console.log(this.tax, 'asxassa')
     this.forFinalcial = false;
+    this.Documentation = false;
+    this.GST = false;
+    this.toRegister = false;
+    //this.tax = false ? this.tax = true : this.tax = false;
     this.tax = !this.tax;
+    this.personalService = false;
+    this.complaince = false;
 
   }
   personalservice() {
     this.forFinalcial = false;
     this.personalService = !this.personalService;
+    this.forFinalcial = false;
+    this.Documentation = false;
+    this.tax = false;
+    this.GST = false;
+    this.toRegister = false;
+    this.complaince = false;
+
 
   }
   complainceandf() {
     this.forFinalcial = false;
+    this.forFinalcial = false;
+    this.Documentation = false;
+    this.GST = false;
+    this.personalService = false;
+    this.forFinalcial = false;
+    this.toRegister = false;
     this.complaince = !this.complaince;
   }
   creativeGallery() {
     this.creativeGall = !this.creativeGall;
     this.logins = false;
     this.forFinalcial = false;
+    this.toScroll = false;
     this.toflip = false;
   }
   brandingSol() {
     this.creativeGall = false;
+    this.digital = false;
+    this.port = false;
     this.brandingSols = !this.brandingSols;
   }
   digitalmarketing() {
     this.creativeGall = false;
+    this.brandingSols = false;
     this.logins = false;
+    this.port = false;
     this.digital = !this.digital;
   }
   portfoliomanagment() {
+    this.digital = false;
     this.creativeGall = false;
     this.logins = false;
+    this.toScroll = false;
+    this.toScroll = false;
     this.port = !this.port;
   }
   login() {
     this.logins = !this.logins;
     this.toflip = false;
+    this.toScroll = false;
 
   }
 }
