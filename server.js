f41f3c39cdd7ba2b4039773c9ff857decfec8587
anshnagrath <anshnagrath448@gmail.com>
2018-10-29 11:42:45 +0530

    for pm2

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,*');
+  next();
+});
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist/ish')))
