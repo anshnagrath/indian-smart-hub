84e15ef09c1abb7f957bbc468d5b895843802694
anshnagrath <anshnagrath448@gmail.com>
2018-11-06 15:45:03 +0530

    aasasaswe232323a

--- server.js
+++ server.js
@@ -1,49 +1,25 @@
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-const logger = require('morgan')
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 4200;
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var router = express.Router();
 
-// Get our API routes
-// const api = require('./server/routes/api');
+/****************************** Router ***************************/
+router.get('*', function(req, res){
+    res.sendFile('index.html', { root: __dirname + '/dist/ish/' });
+});
 
-const app = express();
-// Parsers for POST data
-app.use(logger('dev'));
+/****************************** /Router ***************************/
+
+//app.use(morgan('dev')); // log every request to the console
+app.use(express.static(__dirname + '/dist/ish/')); // Static (public) folder
+
+app.use(bodyParser.urlencoded({extended:true}));// get information from html forms
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(function (req, res, next) {
-   res.setHeader('Content-Type', 'application/json');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type:application/json');
-  next();
-});
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+app.use('/', router); // app.use('/parent', router); call all from localhost:port/parent/*
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist/ish/')))
-console.log(path.join(__dirname, 'dist/ish/'))
-// Set our api routes
-// app.use('/api', api);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/ish/index.html'));
-});
-
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '4200';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+app.listen(port);
