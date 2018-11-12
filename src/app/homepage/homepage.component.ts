21a59f8c75466ae58987b95ba859754f1e6f0782
anshnagrath <anshnagrath448@gmail.com>
2018-11-12 19:02:39 +0530

    msg dialog added

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -10,44 +10,38 @@ import { DOCUMENT } from '@angular/common';
 export class HomepageComponent implements OnInit {
   public toScroll = false; toRegister = false; Documentation = false; complaince = false; brandingSols = false;
   public toflip = false; tax = false; personalService = false; creativeGall = false; digital = false; logins = false;
-  public forFinalcial = false; port = false; creativeClose = false;
+  public forFinalcial = false; port = false; creativeClose = false; isMobile = false;
   public GST = false; smarthubsec = false;
   public Name; lastName; mail; phone; chatboxShow = false;
 
   myFullresImage = 'https://www.file-upload.com/1qr3giyt7l1r';
   public imagesUrl;
-  ngOnInit() {
-    this.imagesUrl = [
-      'https://www.file-upload.com/1qr3giyt7l1r',
-      'https://www.file-upload.com/tf1sxasmakll',
-      'https://www.file-upload.com/abjgx1nqrubw',
-      'https://www.file-upload.com/dshoe34r4piy',
-      'https://www.file-upload.com/d4tzslloi773',
-      'https://www.file-upload.com/2vc1fhbv6iqa',
-      'https://www.file-upload.com/ou1evwlhcqp9',
-      'https://www.file-upload.com/q2ky6363ea35',
-    ];
+  constructor(private _eref: ElementRef, @Inject(DOCUMENT) private document: any) {
   }
-  constructor(private _eref: ElementRef,@Inject(DOCUMENT) private document: any) {
-  } 
-  showChatbox() {
-    console.log('clicked','sadcsadcasdcasdcasdc')
-    this.chatboxShow =!this.chatboxShow;
+  ngOnInit() {
+    (300 < window.screen.width && window.screen.width < 991) ? this.isMobile = true : this.isMobile = false;
+    console.log(window.screen.width, this.isMobile, 'asasaas');
 
   }
- redirectToTwitter() {
+
+  showChatbox() {
+    console.log('clicked', 'sadcsadcasdcasdcasdc')
+    this.chatboxShow = !this.chatboxShow;
+
+  }
+  redirectToTwitter() {
 
     this.document.location.href = 'https://twitter.com/indiansmarthub';
   }
-   redirectToFacebook() {
+  redirectToFacebook() {
 
     this.document.location.href = 'https://www.facebook.com/Indian-Smart-Hub-748135222196160/?modal=admin_todo_tour';
   }
-   redirectToInsta() {
+  redirectToInsta() {
 
     this.document.location.href = 'https://www.instagram.com/indiansmarthub/';
   }
-  ondisplay(){
+  ondisplay() {
     console.log("ascdcasdcasdcsacsadcs")
   }
 }
